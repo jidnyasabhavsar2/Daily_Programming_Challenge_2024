38b3bddcd8eed88c7816f3bf97cae27b8e382653
jidnyasabhavsar2 <118930368+jidnyasabhavsar2@users.noreply.github.com>
2024-09-15 23:59:34 +0530

    Create Day7.cpp

---
+++ Day7.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findtotal(vector<int>arr){
+    int n=arr.size();
+    int lmax=0;
+    int rmax=0;
+    int total=0;
+    int l=0;
+    int r=n-1;
+    while(l<r){
+        if(arr[l] <= arr[r]){
+            if(lmax > arr[l]){
+                total += lmax-arr[l];
+            }
+            else{
+                lmax= arr[l];
+            }
+            l=l+1;
+        }
+        else{
+            if(rmax > arr[r]){
+                total += rmax-arr[r];
+            }
+            else{
+                rmax = arr[r];
+            }
+            r=r-1;
+        }
+    }
+    return total;
+}
+
+int main() {
+	// your code goes here
+    vector<int>arr={0,1,0,2,1,0,1,3,2,1,2,1};
+    int ans = findtotal(arr);
+    cout<<ans;
+}
